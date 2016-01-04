6d4634d408a76b1dd9cc288e8f3b4da26f23dd68
martyjg <martinjosephgormley@gmail.com>
2016-01-04 19:13:25 +0000

    Added information related to the VibeRate display box for the portfolio. wrote reusable function for the back button

--- public/js/app.js
+++ public/js/app.js
@@ -5,22 +5,44 @@ $(function() {
   //   $('.portfolio-overview').css({'height' : portfolioWidth +'px'});
   // });
 
-  $(".about-me-line").hide();
+$(".about-me-line").hide();
+$(".portfolio-overview").hide();
+$(".myspace-display").hide();
+
+$(".line-1").fadeIn(600);
+
+$(".reveal").on("click", function() {
+  var clickedClass = $(this).attr("class").toString();
+  var clickedNumber = clickedClass.split("-").pop();
+  clickedNumber = parseInt(clickedNumber);
+  clickedNumber++;
+  $(".line-" + clickedNumber).fadeIn(600);
+})
+
+$(".open-portfolio").on("click", function() {
+  $(".myspace-display").hide();
+  $(".portfolio-overview").fadeIn(600);
+})
+
+$(".portfolio-item").on("click", function() {
+  var clickedClass = $(this).attr("class").toString();
+  var endingIndex = clickedClass.indexOf("-");
+  var targetItem = clickedClass.substring(0, endingIndex);
+
   $(".portfolio-overview").hide();
+  $("." + targetItem + "-display").fadeIn(600);
 
-  $(".line-1").fadeIn(600);
-
-  $(".reveal").on("click", function() {
-    var clickedClass = $(this).attr("class").toString();
-    var clickedNumber = clickedClass.split("-").pop();
-    clickedNumber = parseInt(clickedNumber);
-    clickedNumber++;
-    $(".line-" + clickedNumber).fadeIn(600);
+    // $(".portfolio-overview").hide();
+    // $(".myspace-display").
   })
 
-  $(".open-portfolio").on("click", function() {
-    $(".portfolio-overview").fadeIn(600);
-  })
+$(".back-button").on("click", function() {
+  var clickedClass = $(this).attr("class").toString();
+  var endingIndex = clickedClass.indexOf("-");
+  var targetItem = clickedClass.substring(0, endingIndex);
+  $("." + targetItem + "-display").hide();
+  $(".portfolio-overview").fadeIn(600);
+})
 
 })
 
